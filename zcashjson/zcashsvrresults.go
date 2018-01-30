65a625f3e5ea26c438f53119feec294e6885b81c
elbandi <bandispam@gmail.com>
2018-01-30 02:10:36 +0100

    Add Params field to ZGetOperationStatusResult

--- zcashjson/zcashsvrresults.go
+++ zcashjson/zcashsvrresults.go
@@ -11,6 +11,21 @@ type ZOperationStatusError struct {
 	Message string `json:"message"`
 }
 
+// ZOperationAmounts models the amounts data in ZOperationParams.
+type ZOperationAmounts struct {
+	Address string  `json:"address"`
+	Amount  float64 `json:"amount"`
+	Memo    string  `json:"memo,omitempty"`
+}
+
+// ZOperationParams models the params data in ZGetOperationStatusResult.
+type ZOperationParams struct {
+	From    string              `json:"fromaddress"`
+	MinConf int                 `json:"minconf"`
+	Amounts []ZOperationAmounts `json:"amounts"`
+	Fee     float64             `json:"fee"`
+}
+
 // ZGetOperationStatusResult models the data from the z_getoperationresult and
 // z_getoperationstatus commands.
 type ZGetOperationStatusResult struct {
@@ -19,6 +34,7 @@ type ZGetOperationStatusResult struct {
 	CreationTime  int                   `json:"creation_time"`
 	Result        map[string]string     `json:"result"`
 	Error         ZOperationStatusError `json:"error"`
+	Params        ZOperationParams      `json:"params"`
 	ExecutionSecs float64               `json:"execution_secs"`
 }
 
